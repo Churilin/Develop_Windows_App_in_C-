a91e3a4b64857bdb2f16c5b16c402f2cbaf0eea7
Vladonchik <116621986+Churilin@users.noreply.github.com>
2024-07-24 00:05:44 +0500

    Add files via upload

---
+++ Windows Form/Lab_02/itmo.CSDesctopApp.WinFormTestList/itmo.CSDesctopApp.WinFormTestList/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace itmo.CSDesctopApp.WinFormTestList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.memberList = new System.Windows.Forms.CheckedListBox();
+            this.peopleList = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonAddData = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.memberList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 145);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список участников";
+            // 
+            // memberList
+            // 
+            this.memberList.FormattingEnabled = true;
+            this.memberList.Location = new System.Drawing.Point(31, 25);
+            this.memberList.Name = "memberList";
+            this.memberList.Size = new System.Drawing.Size(120, 96);
+            this.memberList.TabIndex = 0;
+            // 
+            // peopleList
+            // 
+            this.peopleList.FormattingEnabled = true;
+            this.peopleList.Items.AddRange(new object[] {
+            "Иванов И.И. ",
+            "Петров В.В ",
+            "Сидоров М.М."});
+            this.peopleList.Location = new System.Drawing.Point(12, 163);
+            this.peopleList.Name = "peopleList";
+            this.peopleList.Size = new System.Drawing.Size(121, 28);
+            this.peopleList.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(251, 21);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(111, 43);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(251, 94);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(111, 39);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(251, 207);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(111, 36);
+            this.buttonSort.TabIndex = 4;
+            this.buttonSort.Text = "Сортировка";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonAddData
+            // 
+            this.buttonAddData.Location = new System.Drawing.Point(156, 163);
+            this.buttonAddData.Name = "buttonAddData";
+            this.buttonAddData.Size = new System.Drawing.Size(164, 37);
+            this.buttonAddData.TabIndex = 5;
+            this.buttonAddData.Text = "Загрузить данные";
+            this.buttonAddData.UseVisualStyleBackColor = true;
+            this.buttonAddData.Click += new System.EventHandler(this.buttonAddData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 240);
+            this.Controls.Add(this.buttonAddData);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.peopleList);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Работа со списками";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox memberList;
+        private System.Windows.Forms.ComboBox peopleList;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonAddData;
+    }
+}
+
---
+++ Windows Form/Lab_02/itmo.CSDesctopApp.WinFormTestList/itmo.CSDesctopApp.WinFormTestList/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace itmo.CSDesctopApp.WinFormTestList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (peopleList.Text.Length != 0)
+            {
+                memberList.Items.Add(peopleList.Text);
+            }
+            else MessageBox.Show("Выберите элемент из списка или введите новый");
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            while (memberList.CheckedIndices.Count > 0)
+                memberList.Items.RemoveAt(memberList.CheckedIndices[0]);
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            memberList.Sorted = true;
+        }
+
+        private void buttonAddData_Click(object sender, EventArgs e)
+        {
+            peopleList.Items.Clear();
+            FileStream fStream = new FileStream
+                ("C:\\Обучение ИТМО\\Разработка Windows-приложений на С#\\" +
+                "Выполнение лабораторных\\Lab_02\\itmo.CSDesctopApp.WinFormTestList\\4" +
+                "itmo.CSDesctopApp.WinFormTestList\\XMLData.xml",
+                FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+            XmlDocument xmlDoc = new XmlDocument ();
+            xmlDoc.Load(fStream);
+
+            for (int i = 0; i < xmlDoc.DocumentElement.ChildNodes.Count; i++)
+            {
+                peopleList.Items.Add(xmlDoc.DocumentElement.ChildNodes[i].InnerText);
+
+                fStream.Close();
+            }
+
+        }
+    }
+}
